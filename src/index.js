c38d54ca28fdf7bcca244995526bc284863775f1
Marina-Golovanova <gma18@list.ru>
2021-03-13 20:48:47 +0300

    feat: new functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  debugger;
+  const stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    const added = bracketOpen(stack, bracketsConfig, str[i])
+
+    if (!added) {
+      if (stack[stack.length - 1]?.closing == str[i]) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return stack.length == 0 ? true : false
 }
+
+function bracketOpen(stack, bracketsConfig, el) {
+  for (let j = 0; j < bracketsConfig.length; j++) {
+    if (el == bracketsConfig[j][0]) {
+      if (bracketsConfig[j][1] == bracketsConfig[j][0]) {
+        if (stack.length && stack[stack.length - 1].closing == el) {
+          return false;
+        }
+      }
+      stack.push({
+        opening: el,
+        closing: bracketsConfig[j][1]
+      })
+      return true;
+    }
+  }
+  return false;
+}
+
